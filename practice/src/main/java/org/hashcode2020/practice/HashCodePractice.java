e8df8e18268bcefa069a1f16d6b8eda045a11184
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2020-02-11 07:24:22 +0100

    Pass problem prefix from command line

--- practice/src/main/java/org/hashcode2020/practice/HashCodePractice.java
+++ practice/src/main/java/org/hashcode2020/practice/HashCodePractice.java
@@ -6,13 +6,16 @@ import java.util.concurrent.TimeUnit;
 import org.hashcode2020.Solver;
 import org.hashcode2020.practice.io.PizzaInputReader;
 import org.hashcode2020.practice.io.PizzaOutputWriter;
+import org.hashcode2020.practice.model.PizzaInput;
+import org.hashcode2020.practice.model.PizzaOutput;
+import org.hashcode2020.practice.solvers.AleSolver;
 
-public class HashCode {
+public class HashCodePractice {
 
     private final char problemPrefix;
     private final Solver<PizzaInput, PizzaOutput> solver;
 
-    public HashCode(char problemPrefix) {
+    public HashCodePractice(char problemPrefix) {
         this.problemPrefix = problemPrefix;
         this.solver = new AleSolver();
     }
@@ -50,7 +53,20 @@ public class HashCode {
     }
 
     public static void main(String[] args) {
-        HashCode hashCode = new HashCode('c');
+        char problemPrefix;
+        if (args.length == 1) {
+            String argument = args[0].toLowerCase();
+            if (argument.length() != 1 || argument.compareTo("a") < 0 || argument.compareTo("e") > 0) {
+                throw new IllegalArgumentException("Invalid argument. Only 'a', 'b', 'c', 'd' or 'e' are expected.");
+            }
+            problemPrefix = argument.charAt(0);
+        } else if (args.length == 0) {
+            problemPrefix = 'a';
+        } else {
+            throw new IllegalArgumentException("Too many arguments. Expected at most one argument.");
+        }
+
+        HashCodePractice hashCode = new HashCodePractice(problemPrefix);
         try {
             hashCode.run();
         } catch (Exception e) {
