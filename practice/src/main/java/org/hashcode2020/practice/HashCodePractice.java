413126328e352eecf10e517bfd9ba8adfc15fee1
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2020-02-13 00:21:38 +0100

    Process multiple inputs at the same time

--- practice/src/main/java/org/hashcode2020/practice/HashCodePractice.java
+++ practice/src/main/java/org/hashcode2020/practice/HashCodePractice.java
@@ -12,65 +12,82 @@ import org.hashcode2020.practice.solvers.GreedySolver;
 
 public class HashCodePractice {
 
-    private final char problemPrefix;
+    private final char inputPrefix;
     private final Solver<PizzaInput, PizzaOutput> solver;
 
-    public HashCodePractice(char problemPrefix) {
-        this.problemPrefix = problemPrefix;
+    public HashCodePractice(char inputPrefix) {
+        this.inputPrefix = inputPrefix;
         this.solver = new GreedySolver();
     }
 
     public void run() throws RuntimeException {
         final long startTime = System.nanoTime();
+        System.out.println(String.format("### INPUT '%c' ###", inputPrefix));
 
-        PizzaInput inputData;
+        PizzaInput inputData = readInput();
+        PizzaOutput outputData = solveProblem(inputData);
+        writeOutput(outputData);
+
+        System.out.println(String.format("\nFinal score of this solution is %d.", outputData.getSolutionScore()));
+        System.out.println(String.format("Problem solved in %d ms.", elapsedFrom(startTime)));
+        System.out.println("#################\n");
+    }
+
+    private PizzaInput readInput() {
+        final long startTime = System.nanoTime();
         try {
             System.out.print("Loading input file...");
-            inputData = PizzaInputReader.getInstance().readInputFile(problemPrefix);
-            System.out.println("        OK");
+            PizzaInput inputData = PizzaInputReader.getInstance().readInputFile(inputPrefix);
+            System.out.println(String.format("   \u2713 (%d ms.)", elapsedFrom(startTime)));
+            return inputData;
         } catch (IOException ioe) {
-            System.err.println(String.format("\n\nAn error occurred loading input file '%c'\n", problemPrefix));
+            System.err.println(String.format("\n\nAn error occurred loading input file '%c'\n", inputPrefix));
             throw new RuntimeException(ioe);
         }
+    }
 
+    private PizzaOutput solveProblem(PizzaInput inputData) {
+        final long startTime = System.nanoTime();
         System.out.print("Solving problem...");
         PizzaOutput outputData = solver.solve(inputData);
-        System.out.println("           OK");
+        System.out.println(String.format("      \u2713 (%d ms.)", elapsedFrom(startTime)));
+        return outputData;
+    }
 
+    private void writeOutput(PizzaOutput outputData) {
+        final long startTime = System.nanoTime();
         try {
             System.out.print("Writing solution...");
-            PizzaOutputWriter.getInstance().writeSolution(outputData, problemPrefix);
-            System.out.println("          OK");
+            PizzaOutputWriter.getInstance().writeSolution(outputData, inputPrefix);
+            System.out.println(String.format("     \u2713 (%d ms.)", elapsedFrom(startTime)));
         } catch (IOException ioe) {
             System.err.println("\n\nAn error occurred writing solution to file\n");
             throw new RuntimeException(ioe);
         }
-
-        final long endTime = System.nanoTime();
-        System.out.println(String.format("\nFinal score of this solution is %d.", outputData.getSolutionScore()));
-        System.out.println(String.format("Problem solved in %d ms.",
-                TimeUnit.MILLISECONDS.convert(endTime - startTime, TimeUnit.NANOSECONDS)));
     }
 
-    public static void main(String[] args) {
-        char problemPrefix;
-        if (args.length == 1) {
-            String argument = args[0].toLowerCase();
-            if (argument.length() != 1 || argument.compareTo("a") < 0 || argument.compareTo("e") > 0) {
-                throw new IllegalArgumentException("Invalid argument. Only 'a', 'b', 'c', 'd' or 'e' are expected.");
+    private long elapsedFrom(long startTime) {
+        final long elapsed = System.nanoTime() - startTime;
+        return TimeUnit.MILLISECONDS.convert(elapsed, TimeUnit.NANOSECONDS);
+    }
+
+    public static void main(String[] arguments) {
+        for (String argument : arguments) {
+            if (argument.length() != 1 || argument.compareToIgnoreCase("a") < 0 || argument.compareToIgnoreCase("e") > 0) {
+                throw new IllegalArgumentException(String.format("Invalid argument '%s'. Only 'a', 'b', 'c', 'd' or 'e' are expected.", argument));
             }
-            problemPrefix = argument.charAt(0);
-        } else if (args.length == 0) {
-            problemPrefix = 'a';
-        } else {
-            throw new IllegalArgumentException("Too many arguments. Expected at most one argument.");
+        }
+        if (arguments.length == 0) {
+            throw new IllegalArgumentException("No arguments provided. Expected at least one argument between 'a', 'b', 'c', 'd' or 'e'.");
         }
 
-        HashCodePractice hashCode = new HashCodePractice(problemPrefix);
-        try {
-            hashCode.run();
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
+        for (String inputPrefix : arguments) {
+            try {
+                HashCodePractice hashCode = new HashCodePractice(inputPrefix.charAt(0));
+                hashCode.run();
+            } catch (Exception e) {
+                e.printStackTrace(System.err);
+            }
         }
     }
 
